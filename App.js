1bfebaab505551484a527a8e146894858f66f1c8
Mark <86029787+mj-wd@users.noreply.github.com>
2021-09-03 14:53:18 -0700

    added images

--- App.js
+++ App.js
@@ -5,7 +5,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Aloha! Welcome to The Hawaii Bros' Official Oahu Vacation Planner.</Text>
+      <Text>Do you want to maximize the fun factor on your next visit to this majestical island? Well, bruh, you've come to the right place!</Text>
+      <Text>Other sites bombard you with so many things to do that'll it make your head spin. We want you to just chill, relax, and have no worries. So we've picked the absolute top three things from each of the most popular categories. Trust us, one of us lives here.</Text>
+      <Text>Go ahead and surf the links in the category button below for our #1s if you only have like one weekend or something for your visit.</Text>
       <StatusBar style="auto" />
     </View>
   );
