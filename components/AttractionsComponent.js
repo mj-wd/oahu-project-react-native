44b3a76128ba5f785f441ed13492dd774fdc8773
Mark <86029787+mj-wd@users.noreply.github.com>
2021-09-05 14:08:19 -0700

    building a stack navigator

--- components/AttractionsComponent.js
+++ components/AttractionsComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import Beach from './BeachComponent';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
