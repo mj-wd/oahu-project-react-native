9e173ef0859236e05041f1a878622020b9fb4f77
Mark <86029787+mj-wd@users.noreply.github.com>
2021-09-10 11:59:18 -0700

    centered map on oahu

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -1,16 +1,31 @@
 //by Mark
-//Work in progress with the styling
 import * as React from 'react';
-import MapView from 'react-native-maps';
 import { StyleSheet, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
+
+// export default Class MapMarkers extends React.Component {
+//     state = {
+//         coordinates: [
+//             {name: 'ricados', latitude: '', longitude: ''}
+//         ]
+//     }
+
+// };
 
 export default function Map() {
   return (
     <View style={styles.Container}>
-      <MapView style={styles.Map} />
+      <MapView style={styles.Map} 
+        region={{
+            latitude: 21.48295403007236,
+            longitude: -157.9608539034392,    
+            latitudeDelta: 1.2,
+            longitudeDelta: 0.6
+        }}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
     Container: {
