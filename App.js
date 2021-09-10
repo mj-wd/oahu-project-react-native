5d5173716c92ffe5fff71b5e6658e0e902dd739d
Mark <86029787+mj-wd@users.noreply.github.com>
2021-09-10 11:25:51 -0700

    added google maps

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Main from './components/MainComponent';
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
--- components/MapComponent.js
+++ components/MapComponent.js
@@ -1,54 +1,28 @@
-//by Zach
+//by Mark
 //Work in progress with the styling
-import React, { Component } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View, Dimensions } from 'react-native';
 
-class Map extends Component {
-
-    static navigationOptions = {
-        title: 'Map',
-        tabBarVisible: false
-    }
-
-    render() {
-        return ( 
-            <View style={styles.Container}>
-                <View style={styles.TextBox1}>
-                    <Text style={styles.Text1}>
-                        Will contain a Google Map with pins for the attractions. Please pardon me during the renovation.
-                    </Text>
-                </View>
-                <View style={styles.ImageBox}>
-                    <Image source={require('../assets/images/men-at-work.jpg')} style={styles.Image} />
-                </View>
-            </View>
-        );
-    }
+export default function Map() {
+  return (
+    <View style={styles.Container}>
+      <MapView style={styles.Map} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
     Container: {
-        backgroundColor: '#4EACB8',
-        flex: 1,
-        justifyContent: 'center',
-        padding: 0
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
     },
-    Text1: {
-        fontSize: 22,
-        fontWeight: 'bold',
-        textAlign: 'center'
+    Map: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
     },
-    TextBox1: {
-        justifyContent: 'center'
-    },
-    ImageBox: {
-        backgroundColor: '#4EACB8',
-        alignSelf: 'center'
-    },
-    Image: {
-        width: 350,
-        height: 350,
-    },
-});
+  });
 
-export default Map;
+// export default Map;
