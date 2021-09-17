d874eeb94a27bccb9594494c9773f2bef3f93792
fredodotdesign <85144863+fredodotdesign@users.noreply.github.com>
2021-09-17 08:48:26 -0700

    Updates
    - Links in Fredo bio
    - Working with Flexbox for attractions page

--- components/FredoComponent.js
+++ components/FredoComponent.js
@@ -1,7 +1,8 @@
-//by Zach
+//by Fredo
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Alert, ScrollView, TouchableOpacity, Linking, Image  } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Linking, Image  } from 'react-native';
+import { Icon } from 'react-native-elements';
 
 
 class Fredo extends Component {
@@ -29,6 +30,29 @@ class Fredo extends Component {
             <Image source={require('../assets/images/fredo.jpeg')} style={styles.image} />
             <Text style={styles.bio}>Originally from the Bronx, N.Y., Fredo is an aspiring graphic designer and UX/UI developer, who currently works in retail operations for a major tech company. In his spare time, Fredo enjoys gaming, photography, and training for AIDS/LifeCycle. Fredo lives with his husband John and his two cats in a deee-luxe apartment in the sky in downtown San Francisco, Calif.</Text>
             </TouchableOpacity>
+            <View style={styles.bot}>
+                <Icon
+                    name="instagram"
+                    type="font-awesome"
+                    color="#8134AF"
+                    onPress={() => { Linking.openURL('https://instagram.com/fredodotdesign/')}}
+                    reverse
+                />
+                <Icon
+                    name="twitter"
+                    type="font-awesome"
+                    color="#08a0e9"
+                    onPress={() => { Linking.openURL('https://twitter.com/fredodotdesign')}}
+                    reverse
+                />
+                <Icon
+                    name="mouse-pointer"
+                    type="font-awesome"
+                    color="#E62117"
+                    onPress={() => { Linking.openURL('https://fredo.design')}}
+                    reverse
+                />
+            </View>
         </ScrollView>
     )
 }
@@ -48,6 +72,14 @@ const styles = StyleSheet.create ({
         fontSize: 16,
         fontWeight: "bold",
         textAlign: 'left'
-    }
+    },
+    bot: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-around",
+        flexWrap: "wrap"
+    },
+
 })
 export default Fredo;
